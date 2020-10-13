d3efccc8edf35fcb13b312fd220636f077ae3dd0
fgpacini <fgpacini@yahoo.com>
2020-10-13 01:28:18 -0400

    Basic account creation and login

--- client/pages/api/auth/auth.js
+++ client/pages/api/auth/auth.js
@@ -15,11 +15,41 @@ if(!firebase.apps.length) {
 }
 require("firebase/auth")
 
-export async function _login(data) {
-  var e;
-  firebase.auth().signInWithEmailAndPassword(data["first"], data["last"]).catch(function(error) {
-    console.log(error.message)
-    e = error.message;
+var user = null;
+
+firebase.auth().onAuthStateChanged(function(u) {
+  if(u) {
+    user = u;
+  } else {
+    user = null;
+  }
+})
+
+export async function _create(data) {
+  //return firebase.auth().createUserWithEmailAndPassword(data["email"], data["pwd"]);
+  return new Promise((resolve, reject) => {
+    firebase.auth().createUserWithEmailAndPassword(data["email"], data["pwd"])
+      .then((userCreds) => resolve(userCreds))
+      .catch((reason) => reject(reason));
   });
-  return e;
+}
+
+export function _login(data) {
+  return new Promise((resolve, reject) => {
+    firebase.auth().signInWithEmailAndPassword(data["email"], data["pwd"])
+      .then((userCreds) => resolve(userCreds))
+      .catch((reason) => reject(reason));
+  });
+}
+
+export function getUser(data) {
+  if(user == null || data == null) {
+    return user;
+  } else {
+    for(const [key, value] of Object.entries(data)) {
+      if(user.hasOwnProperty(key)) {
+        data[key] = user[key];
+      }
+    }
+  }
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import * as ui from '@material-ui/core'
+import Alert from '@material-ui/lab/Alert'
 
-import {_login} from './api/auth/auth.js'
+import {_login, _create} from './api/auth/auth.js'
 import { resolveHref } from 'next/dist/next-server/lib/router/router'
 
 export default function Home() {
@@ -101,9 +102,11 @@ class Grid extends React.Component {
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {"first": '', "last": ''};
+    this.state = {"email": '', "pwd": '', "cpwd": '', "error": '', "isLogin": true}; 
+    // Error: 0 is no errors, 1 is incorrect email/pass combo, 2 is password does not match, 3 is invalid email, 4 is 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.switchLogin = this.switchLogin.bind(this)
   }
 
   handleChange(type, event) {
@@ -112,32 +115,102 @@ class Login extends React.Component {
     this.setState(j);
   }
   handleSubmit(event) {
-    (new Promise(function(res, rej) {
-      var e = _login(this.state);
-      console.log(e);
-      if(e) {
-        res("Login successful");
-      }
-      else {
-        rej(e);
-      }
-    })).then(function(res) {
-      this.props.action();
-    }, function(err) {
-      console.log(err);
-    });
-    //alert('A name was submitted: ' + this.state["first"] + " " + this.state["last"]);
+    if(this.state.email == '') {
+      this.setState({"error": "Please enter an email address"});
+    } else if(this.state.pwd == '') {
+      this.setState({"error": "Please enter a password"});
+    } else if(this.state.isLogin) {
+      // try {
+      //   const result  = _login(this.state).then(() => {
+      //     //this.props.action();
+      //   });
+      // } catch (err) {
+      //   console.log(err.message);
+      //   var m = "";
+      //   switch(err.code) {
+      //     case "auth/invalid-email":
+      //       m = "Email address is not valid";
+      //       break;
+      //     case "auth/user-disabled":
+      //       m = "User account has been disabled";
+      //       break;
+      //     case "auth/user-not-found":
+      //     case "auth/wrong-password":
+      //       m = "Incorrect email or password";
+      //       break;
+      //     default:
+      //   }
+      //   if(m) {this.setState({"error": m});}
+      // }
+      _login(this.state).then(val => {
+        this.props.action();
+      }, err => {
+        //console.log(err.message);
+        var m = "";
+        switch(err.code) {
+          case "auth/invalid-email":
+            m = "Email address is not valid";
+            break;
+          case "auth/user-disabled":
+            m = "User account has been disabled";
+            break;
+          case "auth/user-not-found":
+          case "auth/wrong-password":
+            m = "Incorrect email or password";
+            break;
+          default:
+        }
+        if(m) {console.log(m); this.setState({"error": m});}
+      });
+    } else if(this.state.cpwd == '') {
+      this.setState({"error": "Please confirm your password"});
+    } else if(this.state.pwd != this.state.cpwd){ 
+      this.setState({"error": "Passwords do not match"});
+    } else {
+      _create(this.state).then(val => {
+        
+      }, err => {
+        //console.log(err.message);
+        var m = "";
+        switch(err.code) {
+          case "auth/email-already-in-use":
+            m = "Account with given email address already exists";
+            break;
+          case "auth/invalid-email":
+            m = "Email address is not valid";
+            break;
+          case "auth/weak-password":
+            m = "Password is too weak";
+            break;
+        }
+        if(m) {this.setState({"error": m});}
+      });
+    }
     event.preventDefault();
+  }
+
+  switchLogin(event) {
+    this.setState({"isLogin" : !this.state.isLogin});
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <ui.Grid container direction="column" justify="center" alignItems="center">
-          <ui.TextField id="standard-basic" label="Username" onChange={(e) => this.handleChange("first", e)}/>
-          <ui.TextField id="standard-basic" label="Password" onChange={(e) => this.handleChange("last", e)}/>
+          <ui.TextField id="standard-basic" label="Email" onChange={(e) => this.handleChange("email", e)} error={this.state.error == 3} helperText={this.state.error == 3 ? "Invalid email" : ""}/>
+          <ui.TextField id="standard-basic" label="Password" type="password" onChange={(e) => this.handleChange("pwd", e)} error={this.state.error == 1} helperText={this.state.error == 1 ? "Incorrect Username or Password" : ""}/>
+          {!this.state.isLogin && 
+            <ui.TextField id="standard-basic" label="Confirm Password" type="password" onChange={(e) => this.handleChange("cpwd", e)} error={this.state.error == 2} helperText={this.state.error == 2 ? "Password does not match" : ""}/>
+          }
+          {this.state.error && 
+            <Alert severity="error">{this.state.error}</Alert>}
+          <ui.Button disableElevation={true} disableRipple={false} onClick={(e) => this.switchLogin(e)}> 
+            {this.state.isLogin ? "Create an account" : "Log In"}
+          </ui.Button>
+          <ui.Button variant="contained" type="submit"> 
+            Submit
+          </ui.Button>
         </ui.Grid>
-        <input type="submit" value="Submit"/>
       </form>
     );
   }
