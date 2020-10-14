6df0d1ce9ac6b573da5c603eca073e5e061cbb92
fgpacini <fgpacini@yahoo.com>
2020-10-14 16:33:01 -0400

    keys

--- pages/api/auth/auth.js
+++ pages/api/auth/auth.js
@@ -1,15 +1,6 @@
 import * as firebase from "firebase/app";
+import firebaseConfig from "./authkey.json";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCigAOzXJYOEbrWtZBo6ZkvX0SmJf0S4Jg",
-  authDomain: "eatwell-f06d9.firebaseapp.com",
-  databaseURL: "https://eatwell-f06d9.firebaseio.com",
-  projectId: "eatwell-f06d9",
-  storageBucket: "eatwell-f06d9.appspot.com",
-  messagingSenderId: "144128258782",
-  appId: "1:144128258782:web:8696a98f98e36226df084f",
-  measurementId: "G-3DZ83WM9V0"
-};
 if(!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
